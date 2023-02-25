3133295896350fa908c0343f164d6bc04320a0da
JugurtaO <jugurtaourzik@gmail.com>
2023-02-26 00:10:51 +0100

    +master: comments controllers overall done

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -3,13 +3,20 @@ const postsRouter = express.Router();
 
 const postsControllers = require("../controllers/posts/index");
 const { sanitize } = require("../middlewares/sanitization/sanitize");
+const commentsControllers=require("../controllers/comments/index");
 
 postsRouter.get("/:post_id", sanitize, postsControllers.viewPost);
+postsRouter.get("/:post_id/comments",sanitize,commentsControllers.viewPostComments);
+postsRouter.get("/:post_id/comments/:comment_id",sanitize,commentsControllers.viewComment);
+
 
 
 postsRouter.post("/add", sanitize, postsControllers.addPost);
 postsRouter.post("/:post_id/edit", sanitize, postsControllers.editPost);
 postsRouter.post("/:post_id/delete", sanitize, postsControllers.deletePost);
+postsRouter.post("/:post_id/comments/add",sanitize,commentsControllers.addComment);
+postsRouter.post("/:post_id/comments/:comment_id/delete",sanitize,commentsControllers.deleteComment);
+
 
 
 module.exports = postsRouter;
