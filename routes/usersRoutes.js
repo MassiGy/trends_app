9e1b78660aabf568e5008081aec22650c5f1fa9e
MassiGy <ghernaoutmassi@gmail.com>
2023-03-04 23:40:30 +0100

    +dev-massigy: added createdAt and updatedAt timestamps to the tables

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -15,7 +15,7 @@ usersRouter.get("/:user_id/comments/:comment_id",sanitize,commentsControllers.vi
 usersRouter.post("/register", sanitize, usersControllers.register);
 usersRouter.post("/:user_id/edit", sanitize, usersControllers.editProfile);
 usersRouter.post("/:user_id/signout", sanitize, usersControllers.signout);
-postsRouter.post("/:user_id/comments/:comment_id/edit", sanitize, commentsControllers.editComment);
+usersRouter.post("/:user_id/comments/:comment_id/edit", sanitize, commentsControllers.editComment);
 usersRouter.post("/:user_id/comments/:comment_id/delete",sanitize,commentsControllers.deleteComment);
 
 
