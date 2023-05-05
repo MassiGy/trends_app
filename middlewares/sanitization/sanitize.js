09e7f4fdb6cf53f0d0207ef96aeaba6a9e296004
MassiGy <ghernaoutmassi@gmail.com>
2023-05-05 16:22:52 +0200

    +dev-massigy: fixed bug on sanitze.js

--- middlewares/sanitization/sanitize.js
+++ middlewares/sanitization/sanitize.js
@@ -1,16 +1,21 @@
 
 module.exports.sanitize = (req, res, next) => {
-    
+
     Object.keys(req.body).forEach(key => {
-        if(req.body[String(key)] instanceof String)
+        if (isNaN(req.body[String(key)])) // if it is a string
+        {
             req.body[String(key)] = req.body[String(key)].replace(/['`";\$%\*]/g, "_");
+        }
     });
-    
+
     Object.keys(req.params).forEach(key => {
-        if(req.params[String(key)] instanceof String)
+        if (isNaN(req.params[String(key)]))   // if it is a string 
+        {
             req.params[String(key)] = req.params[String(key)].replace(/['`";\$%\*]/g, "_");
+        }
+
     });
-   
+
 
     next();
 }
